1579e710bb29888de34cba8b04bb01e15be5a35e
Dani <danieletortoli@gmail.com>
2019-10-23 11:37:06 +0200

    SAX Parser configuration

--- parser.py
+++ parser.py
@@ -8,7 +8,9 @@ venue = ['book', 'inproceedings']
 
 class PublicationHandler(xml.sax.ContentHandler):
     """Class for handle parsing events and adding documents to the publication index"""
-    parentflag = False  # to check
+
+    # variable that defines a publication document
+    isPublication = False
 
     # attributes list
     key = ''
@@ -21,10 +23,23 @@ class PublicationHandler(xml.sax.ContentHandler):
 
     # -----------
     __CurrentElement = None
+    writer = None
 
     def __init__(self, writer):
+        self.__reset()
         self.writer = writer
         super(PublicationHandler, self).__init__()  # parent init
+
+    def __reset(self):
+        """A function for resetting the class attributes"""
+        self.isPublication = False
+        self.key = ''
+        self.tag = ''
+        self.author = ''
+        self.title = ''
+        self.crossref = ''
+        self.pages = ''
+        self.year = ''
 
     def startDocument(self):
         """Called when the XML Parser starts reading the file"""
@@ -32,11 +47,148 @@ class PublicationHandler(xml.sax.ContentHandler):
 
     def endDocument(self):
         """Called when the parsing is completed"""
-        print('Parsing completed.\n')
+        print('Publication parsing completed.\n')
 
     def startElement(self, tag, attributes):
+        """Called when a publication is parsed"""
         self.__CurrentElement = tag
-        
-
+        for element_tag in publication:
+            if tag == element_tag:
+                self.isPublication = True
+                self.key = str(attributes['key'])
+                self.tag = tag
 
     def endElement(self, tag):
+        """Called when the parsing of the publication ends"""
+        if self.tag == tag:
+            self.writer.add_document(pubtype=self.tag,
+                                     key=self.key,
+                                     author=self.author,
+                                     title=self.title,
+                                     pages=self.pages,
+                                     crossref=self.crossref,
+                                     year=self.year)
+
+            self.__reset()
+
+    def characters(self, content):
+        """Called to assign the attributes of a publication document"""
+        if self.isPublication:
+            if self.__CurrentElement == 'author':
+                self.author += str(content)
+            elif self.__CurrentElement == "title":
+                self.title += str(content)
+            elif self.__CurrentElement == "pages":
+                self.pages += str(content)
+            elif self.__CurrentElement == "crossref":
+                self.crossref += str(content)
+            elif self.__CurrentElement == "year":
+                self.year += str(content)
+
+
+class VenueHandler(xml.sax.ContentHandler):
+    """Class for handle parsing events and adding documents to the venue index"""
+
+    # variable that defines a venue document
+    isVenue = False
+
+    # attributes list
+    key = ''
+    tag = ''
+    author = ''
+    title = ''
+    journal = ''
+    publisher = ''
+    parent = False
+
+    # -----------
+    __CurrentElement = None
+    writer = None
+
+    def __init__(self, writer):
+        self.__reset(3)
+        self.writer = writer
+        super(VenueHandler, self).__init__()  # parent init
+
+    def __reset(self, selector):
+        """A function for resetting the class attributes.
+            Selector is used for the bitwise and:
+            1 to enter the first condition
+            2 to enter the second condition
+            3 to enter both of them"""
+
+        self.key = ''
+        self.tag = ''
+
+        if selector & 1:
+            self.isVenue = False
+            self.author = ''
+            self.title = ''
+            self.publisher = ''
+
+        if selector & 2:
+            self.journal = ''
+            self.parent = False
+
+    def startDocument(self):
+        """Called when the XML Parser starts reading the file"""
+        print('Venue indexing started.\n')
+
+    def endDocument(self):
+        """Called when the parsing is completed"""
+        print('Venue parsing completed.\n')
+
+    def startElement(self, tag, attributes):
+        """Called when a venue is parsed"""
+        self.__CurrentElement = tag
+        if tag in venue:
+            for element_tag in venue:
+                if tag == element_tag:
+                    self.isVenue = True
+                    self.key = str(attributes['key'])
+                    self.tag = tag
+
+        elif tag in publication:
+            for element_tag in publication:
+                if tag == element_tag:
+                    self.parent = True
+                    self.key = str(attributes['key'])
+                    self.tag = tag
+
+    def endElement(self, tag):
+        """Called when the parsing of the venue ends"""
+        if tag in venue:
+            for element_tag in venue:
+                if tag == element_tag:
+                    self.writer.add_document(pubtype=self.tag,
+                                             key=self.key,
+                                             author=self.author,
+                                             title=self.title,
+                                             publisher=self.publisher
+                                             )
+                    self.__reset(1)
+
+        if tag in publication:
+            for element_tag in publication:
+                if tag == element_tag:
+                    self.writer.add_document(pubtype=self.tag,
+                                             key=self.key,
+                                             journal=self.journal
+                                             )
+                    self.__reset(2)
+
+    def characters(self, content):
+        """Called to assign the attributes of a venue document"""
+        if self.isVenue:
+            if self.__CurrentElement == "author":
+                self.author += str(content)
+
+            elif self.__CurrentElement == "title":
+                self.title += str(content)
+
+            elif self.__CurrentElement == "publisher":
+                self.publisher += str(content)
+
+            elif self.parent:
+                if self.__CurrentElement == 'journal':
+                    self.journal += content
